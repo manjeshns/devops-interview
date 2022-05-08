10b02bfc1076bb32e179c00117dd80f9c2881fea
manjeshns <35796218+manjeshns@users.noreply.github.com>
2022-05-08 22:42:59 +0100

    Update IWeatherClient.cs

--- Src/Core/BradyWeather.Application/Interfaces/IWeatherClient.cs
+++ Src/Core/BradyWeather.Application/Interfaces/IWeatherClient.cs
@@ -7,10 +7,10 @@ namespace BradyWeather.Application.Interfaces
 {
     public interface IWeatherClient
     {
-        [Get("/locations/v1/cities/autocomplete?apikey={apikey}&q={search}")]
+        [Get("/locations/v1/cities/autocomplete?apikey=zgpECC1AO3Z6tpYjyqf8YM1QAsLLlXTN&q={search}")]
         Task<Location[]> GetLocationsByText(string apiKey, string search, CancellationToken cancellationToken);
 
-        [Get("/currentconditions/v1/{locationKey}?apikey={apikey}&details=true")]
+        [Get("/currentconditions/v1/{locationKey}?apikey=zgpECC1AO3Z6tpYjyqf8YM1QAsLLlXTN&details=true")]
         Task<Forecast[]> GetWeatherForecast(string apiKey, string locationKey, CancellationToken token);
     }
 }
